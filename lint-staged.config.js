9d78a09983907731aa4e310263e3d2b01a2be480
Tomáš Szabo <tomas.szabo@deftomat.com>
2018-10-22 18:12:54 +0200

    Disable tslint --project for js files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
+  '*.{ts,tsx}': [
     'tslint --config tslint.fix.json --project tsconfig.json --fix',
     'prettier --write',
     'git add'
   ],
+  '*.{js,jsx}': ['tslint --config tslint.fix.json --fix', 'prettier --write', 'git add'],
   '*.{css,scss,json,yml,graphql,md}': ['prettier --write', 'git add']
 };
